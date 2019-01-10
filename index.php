92c6350762bfb01673145b8691cf942f4f2b3f2c
kin210308 <45562828+kin210308@users.noreply.github.com>
2019-01-10 23:28:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  	<head>
+	 	<meta charset="utf-8">
+	 	<link rel="stylesheet" type="text/css" href="style.css">
+	 	<link href="https://fonts.googleapis.com/css?family=Crete+Round" rel="stylesheet">
+		<title>MonCV</title>
+	</head>	
+	<body>
+	 	<header>
+	 		<h1> Karine Basire</h1>
+	 	<?php echo date("d m Y H:i"); ?>
+	 		<p> 0660000000, Cannes</p>
+	 	</header>
+	 	<section id="partie1">
+	 		<h2> FORMATION<br><strong>de codage</h2>
+	 		<a href="#but" class="button">En attente</a>
+	 	</section>
+	 	<section id="partie2">
+	 			<div id="restauration">
+	 			<h4>Restauration </h4>
+	 			<p><large>(15 ans)</large></p><br>
+	 			<p>La polyvalence, travail d'équipe,<br> resistance au stress,<br> adaptation, courtoisie,<br> savoir vivre, rigueur,<br> respect des protocoles.</p>
+	 		</div>
+	 		<div id="metier">
+	 			<h4>Gérante SARL</h4>
+	 			<p><large>(3 ans)</large></p><br>
+	 			<p>L’autonomie,<br> gestion personnel,<br> organisation, empathie,<br> sens de l’écoute,<br> capacité à gérer des conflits.</p>
+	 		</div>
+	 	</section>
+		<section id="steps">
+			<ul>
+				<li id=step1>
+					<h4>Autres Compétences</h4>
+					<p> Informatique : word, excel.<br>Connaissance normes HACCP.<br>Comptabilité</p>
+				</li>
+				<li id=step2>
+					<h4>Langues parlées</h4>
+					<p>Anglais :<br> pratique occasionnelle<br>Espagnol : <br>scolaire</p>
+				</li>
+				<li id=step3>
+					<h4>Centre d'intêrets</h4>
+					<p>Passionnée de lecture<br>Pratique du piano<br>
+					Décoration interieure<br>Informatique</p>
+				</li>
+			</ul>
+	 	</section>
+	 	<section id="contact">
+	 			<h3> FORMATION</h3>
+	 			<p> Secrétariat, comptabilité général, HACCP.</p>
+	 	</section>			
+		<footer>
+			<h1>basire.karine<span class="rouge">@</span>gmail.com</span></h1>	
+		</footer>
+	</body>
+</html>
